9afe9801e292b0e7700f00639fcac6aea583ffe2
Marc Devaney <marc@clubinvictus.com>
2026-04-22 14:03:22 +0530

    fix: redundant div tags causing JSX error

--- src/components/CalendarView.tsx
+++ src/components/CalendarView.tsx
@@ -352,8 +352,6 @@ export const CalendarView = () => {
                         )}
                     </div>
                 </div>
-                    </div>
-                </div>
             </header>
 
             {viewMode === 'week' ? (
